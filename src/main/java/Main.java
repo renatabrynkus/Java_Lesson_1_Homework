8686bf1cf95445da93938cc38533f0a0d5787282
rbrynkus <renata.brynkus@gmail.com>
2022-08-05 11:41:41 +0200

    Added class Main with the solution.

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numberToGuess = new Random().nextInt(100);
+
+        System.out.println("Please type an integer between 0 and 99.");
+
+        Scanner scan = new Scanner(System.in);
+        int remainingAttempts = 5;
+
+        while (remainingAttempts > 0) {
+            int integerFromUser = scan.nextInt();
+
+            if (integerFromUser < numberToGuess) {
+                System.out.println("Your number is LOWER than the one you are trying to guess.");
+                remainingAttempts--;
+                printRemainingAttempts(remainingAttempts);
+            } else if (integerFromUser > numberToGuess) {
+                System.out.println("Your number is GREATER than the one you are trying to guess.");
+                remainingAttempts--;
+                printRemainingAttempts(remainingAttempts);
+            } else {
+                System.out.println("You guessed it!");
+                break;
+            }
+
+            if (remainingAttempts != 0) {
+                System.out.println("Please try again.");
+            } else {
+                System.out.println("Sorry, you didn't guess the number. The answer was: " + numberToGuess);
+            }
+        }
+    }
+
+    public static void printRemainingAttempts(int remainingAttempts) {
+        System.out.println("Attempts remaining: " + remainingAttempts);
+    }
+}
